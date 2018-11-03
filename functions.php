f30c74ccd8c72d2b1c2b4123b2ffa7684e5014ec
Dimitris Naziris <nazdim@gmail.com>
2018-11-03 23:48:56 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -5,28 +5,15 @@
 		$query = "select airline from airlines where iata = ?";
 	
 		if ($stmt = $mysqli->prepare($query)) {
-
-			/* bind parameters for markers */
 			$stmt->bind_param("s", $airline_code);
-
-			/* execute query */
 			$stmt->execute();
-
-			/* bind result variables */
 			$stmt->bind_result($airline);
-
-			/* fetch value */
 			$stmt->fetch();
 
-			
-
-			/* close statement */
 			$stmt->close();
 		}
 
-		/* close connection */
 		$mysqli->close();
-
 	
 		return $airline;
 	}
@@ -37,43 +24,18 @@
 		$query = "select city, country from airports where code = ?";
 	
 		if ($stmt = $mysqli->prepare($query)) {
-
-			/* bind parameters for markers */
 			$stmt->bind_param("s", $airport_code);
-
-			/* execute query */
 			$stmt->execute();
-
-			/* bind result variables */
 			$stmt->bind_result($city, $country);
-
-			/* fetch value */
 			$stmt->fetch();
 
-			
-
-			/* close statement */
 			$stmt->close();
 		}
 
-		/* close connection */
 		$mysqli->close();
 
-		//$airport = ["city"->$city, "country"->$country];
 		$airport = $city.",<br>".$country;
 		return $airport;
-		
-		
-		/*
-		$airport_request = 'https://api.sandbox.amadeus.com/v1.2/airports/autocomplete?apikey=3jl39kYzxVtbQA9UWNg9BTV5Su3vLGnu&term='.$airport_code;
-		$airport_response  = file_get_contents($airport_request);
-		$airport_jsonobj  = json_decode($airport_response);
-		
-		foreach($airport_jsonobj as $airport_result)
-		{
-			if($airport_result->value == $airport_code) return $airport_result->label;
-		}
-		return $airport;*/
 	}
 	
 	
@@ -82,11 +44,9 @@
 		if(is_array($http_response_header))
 		{
 			$parts=explode(' ',$http_response_header[0]);
-			if(count($parts)>1) //HTTP/1.0 <code> <text>
-				return intval($parts[1]); //Get code
+			if(count($parts)>1)
+				return intval($parts[1]);
 		}
 		return 0;
 	}
-	
-	
 ?>
