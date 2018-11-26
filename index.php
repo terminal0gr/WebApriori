81c0f541ac62e2c6cd6612189f68850bc9803cf8
Kostas Malliaridis <malliaridis@mentor.com.gr>
2018-11-26 13:58:25 +0200

    Login & SignUp
    Login & SignUp in different pages

--- index.php
+++ index.php
@@ -12,8 +12,7 @@ session_destroy();
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     
     <!-- Bootstrap CSS -->
-	  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"> -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
     <link rel="icon" type="image/png" href="./images/plane_02.png"/>
     
@@ -25,8 +24,6 @@ session_destroy();
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-    <!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> -->
-
 
     <title>Flight Scanner V1.1</title>
 
@@ -46,10 +43,10 @@ session_destroy();
 		</ul>
 		<ul class="navbar-nav ml-auto">
 			<li class="nav-item">
-				<a class="nav-link">Εγγραφή</a>
+				<a class="nav-link" href="SignUp.html">Εγγραφή</a>
 			</li>
 			<li class="nav-item">
-				<a class="nav-link" >Είσοδος</a>
+				<a class="nav-link" href="Login.html">Είσοδος</a>
 			</li>
 			<li class="nav-item">
 				<a class="nav-link" href = "logout.php">Έξοδος</a>
