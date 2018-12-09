3a36946f671236eeef72250c559bccfc520bf1db
Malliaridis Konstantinos <terminal_gr@yahoo.com>
2018-12-09 13:48:00 +0200

    Token manipulation 2 (Not Ready)
    Θα συνεχίζω το βράδυ...

---
+++ scripts/login.js
@@ -0,0 +1,59 @@
+$(function() {
+
+	// Get the form.
+	var form = $('#ajax-contact');
+
+	// Get the messages div.
+	var formMessages = $('#form-messages');
+
+	// Set up an event listener for the contact form.
+	$(form).submit(function(e) {
+		// Stop the browser from submitting the form.
+		e.preventDefault();
+
+		// Serialize the form data.
+		var formData = $(form).serialize();
+
+		// Submit the form using AJAX.
+		$.ajax({
+			type: 'POST',
+			url: $(form).attr('action'),
+			data: formData
+		})
+		.done(function(response) {
+			// Make sure that the formMessages div has the 'success' class.
+			$(formMessages).removeClass('error');
+			$(formMessages).addClass('success');
+
+			res=JSON.parse(response);
+
+			if (res.http_response_code==200) {
+				$(formMessages).text(res.token + '<br>' + res.http_response_code + ' ' + res.message);
+				sessionStorage.setItem('token', res.token);
+				sessionStorage.setItem('username', res.name);
+				window.location.href='index.html';
+			} else {
+				$(formMessages).text('An error occured.<br>' + res.http_response_code + ' ' + res.message)
+
+			}
+
+			// Clear the form.
+			//$('#name').val('');
+			//$('#email').val('');
+			//$('#message').val('');
+		})
+		.fail(function(data) {
+			// Make sure that the formMessages div has the 'error' class.
+			$(formMessages).removeClass('success');
+			$(formMessages).addClass('error');
+
+			if (data.responseText) {
+				$(formMessages).text(data.responseText);
+			} else {
+				$(formMessages).text('An error occured.');
+			}
+		});
+
+	});
+
+});
--- validation.php
+++ validation.php
@@ -2,39 +2,66 @@
 	include("./config.php");
 	include("./phpfunctions/jwt_helper.php");
 	session_start(); 
+    
+	if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+		$con = new mysqli(HOST, USERNAME, PWD, DB);
+		//$con = mysqli_connect("localhost", "webeng7", "webeng71819", "webeng7");
 	
-   $con = new mysqli(HOST, USERNAME, PWD, DB);
-   //$con = mysqli_connect("localhost", "webeng7", "webeng71819", "webeng7");
- 
-   /* check connection */
-	if (mysqli_connect_errno()) {
-		printf("<BR>Fatal Error: %s\n", mysqli_connect_error());
-		exit();
+		/* check connection */
+		if (mysqli_connect_errno()) {
+			printf("<BR>Fatal Error: %s\n", mysqli_connect_error());
+			exit();
+		}
+
+        //The FILTER_SANITIZE_EMAIL filter removes all illegal characters from an email address
+        $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+        //mysqli_real_escape_string function is used to create a legal SQL string that you can use in an SQL statement.
+		$email = mysqli_real_escape_string($con, $_POST['email']);
+		$passwd = mysqli_real_escape_string($con, $_POST['passwd']);
+        
+        if ( empty($email) OR empty($passwd) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            // Set a 400 (bad request) response code and exit.
+            http_response_code(400);
+            echo "There was a problem with your submission. Please complete the form and try again.";
+            exit;
+        }
+    
+        $passwd=md5($passwd);
+		$s="SELECT * FROM usertable WHERE email='$email' and passwd='$passwd'";
+		$result = mysqli_query($con,$s);
+		$num=mysqli_num_rows($result);
+
+		// If result matched $myusername and $mypassword, table row must be 1 row
+		if($num==1){
+
+            $rowobj = $result->fetch_object();
+
+			$token = array();
+			$token['id'] = $email;
+			//$etoken = JWT::encode($token, SERVERKEY); //SERVERKEY from config.php
+
+            $JsonReq = array('http_response_code' => 200, 'token' => JWT::encode($token, SERVERKEY), 'name' => $rowobj->fname." ".$rowobj->oname, 'message' => 'success');
+            print json_encode($JsonReq);
+
+			//header("location:search.html");
+		} else {
+			
+            $JsonReq = array('http_response_code' => 500, 'message' => 'Username and/or Password incorrect.\\nTry again.');
+            print json_encode($JsonReq);
+
+            //$message = "Username and/or Password incorrect.\\nTry again.";
+			//echo "<script type='text/javascript'>alert('$message');
+			//window.location.href='Login.html';</script>";
+            //echo $message;
+
+		}
+
+	} else {
+		// Not a POST request, set a 403 (forbidden) response code.
+		http_response_code(403);
+		echo "There was a problem with your submission, please try again.";
 	}
-   
-   $email = mysqli_real_escape_string($con, $_POST['email']);
-   $passwd = mysqli_real_escape_string($con, $_POST['passwd']);
- 
-   $s="SELECT * FROM usertable WHERE email='$email'&& passwd=md5('$passwd')";
-   $result = mysqli_query($con,$s);
-   $num=mysqli_num_rows($result);
-   
-	// If result matched $myusername and $mypassword, table row must be 1 row
-   if($num==1){
-
-	   $token = array();
-	   $token['id'] = $email;
-	   $_SESSION["encodedToken"] = JWT::encode($token, SERVERKEY); //SERVERKEY from config.php
-
-	   header("location:search.html");
-	}else{
-		
-		$message = "Username and/or Password incorrect.\\nTry again.";
-		echo "<script type='text/javascript'>alert('$message');
-		window.location.href='Login.html';</script>";
-		echo $message;
-
-	}
-//	session_destroy();
+	//	session_destroy();
 ?>
 
