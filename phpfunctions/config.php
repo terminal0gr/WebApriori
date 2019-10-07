c1ed0496cc8d747a0017d8218ca2aa919c5f05bc
Malliaridis Konstantinos <terminal_gr@yahoo.com>
2019-10-07 23:36:59 +0300

    7/10/2019
    Web interface jobs

--- phpfunctions/config.php
+++ phpfunctions/config.php
@@ -2,9 +2,9 @@
 	//define( 'USERNAME', 'webeng7' );
 	//define( 'PWD', 'webeng71819' );   
 	//define( 'DB', 'webeng7' );   
-	define( 'USERNAME', 'flightsuser' );
-	define( 'PWD', 'flightsuser' );   
-	define( 'DB', 'flightsdata' );   
+	define( 'USERNAME', 'aprioriUser' );
+	define( 'PWD', 'aprioripwd' );   
+	define( 'DB', 'aprioriBase' );   
 	define( 'HOST', 'localhost' ); 
 	define( 'SERVERKEY', 'e486n8wn76v7n69v7as678z7n674b7zx7vb7' ); 
 	define( 'apikey', 'TZXCihIgRAkexLGxXtsVmfuyJpwHlp82');
