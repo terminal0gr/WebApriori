f22854798a2878635fb2c3a5bc409df6c4620b57
Malliaridis Konstantinos <terminal_gr@yahoo.com>
2019-10-12 00:59:09 +0300

    Mailer not working...
    Mailer not working...

--- phpfunctions/registration.php
+++ phpfunctions/registration.php
@@ -1,13 +1,21 @@
 <?php
 	include("config.php");
-	
+
+	//Import the PHPMailer class into the global namespace
+	use PHPMailer\PHPMailer\PHPMailer;
+	use PHPMailer\PHPMailer\SMTP;
+	use PHPMailer\PHPMailer\Exception;
+	require '.\PHPMailer\src\PHPMailer.php';
+	require '.\PHPMailer\src\Exception.php';
+	require '.\PHPMailer\src\SMTP.php';
+
 	session_start();
 	$con1 = new mysqli(HOST, USERNAME, PWD, DB);
 	
 	/* check connection */
 	if ($con1->connect_error) {
 		http_response_code(403);
-		$JsonReq = array('http_response_code' => 201, 'title' => 'Error', 'message' => 'Connection Error');
+		$JsonReq = array('http_response_code' => 403, 'title' => 'Error', 'message' => 'Connection Error');
 		print json_encode($JsonReq);
 		die("Connection Error" . $con1->connect_error);
 	}
@@ -52,44 +60,68 @@
 	/* User is registered */
 	if($num==1) {
 		http_response_code(201);
-		$JsonReq = array('http_response_code' => 201, 'title' => 'Warning', 'message' => 'Sorry...You are already registered user...');
+		$JsonReq = array('http_response_code' => 201, 'title' => 'Information', 'message' => 'You are already registered user!');
 		print json_encode($JsonReq);
-	} elseif(!$captcha_error) { /* New user @temp_usertable & send confirmation email */
-		$sql="INSERT INTO temp_usertable(confirm_code,temail,tpasswd,toname,tfname)VALUES('$confirm_code','$email','$passwd','$oname','$fname')";
-		$result = mysqli_query($con1,$sql);
-		
-		if($result) {
+		exit();
+	} 
+	elseif(!$captcha_error) { /* New user @temp_usertable & send confirmation email */
+
+		$query ="SELECT * FROM temp_usertable WHERE temail='$email'";
+		$result = mysqli_query($con1,$query);
+		$num=mysqli_num_rows($result);
+		if($num==0) {
+
 			$message = "Your Confirmation link \r\nhttp://nireas.it.teithe.gr/webeng7/flights/phpfunctions/confirmation.php?passkey=$confirm_code ";                   
-			require_once("class.phpmailer.php");
-			$mail = new PHPMailer;
-			$mail -> IsSMTP();
+
+			$mail = new PHPMailer(TRUE);
+			$mail->IsSMTP();
 			$mail->CharSet="UTF-8";     
-			$mail->Host = 'smtp.teithe.gr';
-			$mail->Port = 25;       
-			$mail->From = 'ait152018@ait.teithe.gr';   
-			$mail->FromName = 'Rules mining';
+
+			//$mail->Host = 'smtp.teithe.gr';
+			//$mail->Port = 25;       
+			$mail->Host = 'smtp.mail.yahoo.com';
+			$mail->Port = 465; 
+			$mail->SMTPAuth = true;                 // Enable SMTP authentication
+            $mail->Username = 'terminal_gr@yahoo.com';  // SMTP username
+            $mail->Password = 'samall321&@!';    // SMTP password
+            $mail->SMTPSecure = 'ssl'; 
+
+			$mail->setFrom('terminal_gr@yahoo.com', 'Association Rules mining');   
 			$mail->addAddress($email);               
 			$mail->Subject  = "Association rules mining. Please confirm your verification!";
 			$mail->Body  = $message;
-			if(!$mail->send()) {
 
-				http_response_code(403);
-				$JsonReq = array('http_response_code' => 201, 'title' => 'Error', 'message' => 'Email has not been sent.' + $mail->ErrorInfo);
+			if(!$mail->send()) {
+                echo "Mailer Error: " . $mail->ErrorInfo;
+				http_response_code(401);
+				$JsonReq = array('http_response_code' => 401, 'title' => 'Error', 'message' => 'Email has not been sent. '.$mail->ErrorInfo);
 				print json_encode($JsonReq);
 				exit();
-			} else {			  
+			} 
 
+		    $sql="INSERT INTO temp_usertable(confirm_code,temail,tpasswd,toname,tfname)VALUES('$confirm_code','$email','$passwd','$oname','$fname')";
+		    $result = mysqli_query($con1,$sql);
+		    if($result) {
 				http_response_code(200);
 				$JsonReq = array('http_response_code' => 200, 'title' => 'Information', 'message' => 'Your Confirmation link Has Been Sent To Your Email Address.');
 				print json_encode($JsonReq);
 				exit();
-
 			}
-		} else { /* Confirmation email has been already sent */
-			http_response_code(403);
+			else {
+				http_response_code(401);
+				$JsonReq = array('http_response_code' => 401, 'title' => 'Error', 'message' => 'Database error!!!');
+				print json_encode($JsonReq);
+				exit();
+			}
+
+
+		}
+		else { /* Confirmation email has been already sent */
+
+			http_response_code(201);
 			$JsonReq = array('http_response_code' => 201, 'title' => 'Information', 'message' => 'Your confirmation link has been already sent to your email address '.$email.'. Please check your email again!');
 			print json_encode($JsonReq);
-			exit();
+			exit;
 
 		}
 	}
