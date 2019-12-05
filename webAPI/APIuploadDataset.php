f3562c3191fdf196e2031f6439b195db68dfcc76
Malliaridis Konstantinos <terminal_gr@yahoo.com>
2019-12-06 00:24:03 +0200

    API upload dataset 02
    API upload dataset 02

--- webAPI/APIuploadDataset.php
+++ webAPI/APIuploadDataset.php
@@ -1,20 +1,20 @@
 <?php
-    include("config.php");
+    include("../phpfunctions/config.php");
 
     session_start(); 
     
     $auth = false;
-	
-    if (!isset($_FILES['fileToUpload'])) {
+    
+    if (!isset($_POST['email'])) {
         http_response_code(400);
-        $JsonReq = array('code' => 10, 'message' => 'No file for upload detected!!!');
+        $JsonReq = array('code' => 20, 'message' => 'Unauthorized!!!');
         print json_encode($JsonReq);
         exit();
     }
 
-    if (!isset($_POST['email'])) {
+    if (!isset($_FILES['fileToUpload'])) {
         http_response_code(400);
-        $JsonReq = array('code' => 20, 'message' => 'Unauthorized!!!');
+        $JsonReq = array('code' => 10, 'message' => 'No file for upload detected!!!');
         print json_encode($JsonReq);
         exit();
     }
@@ -29,13 +29,6 @@
     if (!isset($_POST['datasetType'])) {
         http_response_code(400);
         $JsonReq = array('code' => 40, 'message' => 'Dataset type not provided!!!');
-        print json_encode($JsonReq);
-        exit();
-    }
-
-    if (!isset($_POST['fileName'])) {
-        http_response_code(400);
-        $JsonReq = array('code' => 50, 'message' => 'No filename Provided!!!');
         print json_encode($JsonReq);
         exit();
     }
@@ -59,8 +52,8 @@
     //The FILTER_SANITIZE_EMAIL filter removes all illegal characters from an email address
     $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
     //mysqli_real_escape_string function is used to create a legal SQL string that you can use in an SQL statement.
-	$email = mysqli_real_escape_string($con, $_POST['email']);
-    $apiKey = mysqli_real_escape_string($con, $_POST['apiKey']);
+	$email = mysqli_real_escape_string($con1, $_POST['email']);
+    $apiKey = mysqli_real_escape_string($con1, $_POST['apiKey']);
     $identity=md5($email);
 
     if (empty($email) OR empty($apiKey) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
@@ -68,6 +61,29 @@
         $JsonReq = array('code' => 80, 'message' => 'Unauthorized!!!');
         print json_encode($JsonReq);
         exit;
+    }
+
+    $s="SELECT key_created_at FROM usertable 
+        WHERE email='$email' and webAPIKey='$apiKey'";
+    $result = mysqli_query($con1,$s);
+    $num=mysqli_num_rows($result);
+
+    // If result matched $myusername and $mypassword, table row must be 1 row
+    if(!$num==1){
+        http_response_code(400);
+        $JsonReq = array('code' => 90, 'message' => 'Unauthorized!!!');
+        print json_encode($JsonReq);
+        exit; 
+    }
+
+    $rowobj = $result->fetch_object();
+    $dateTimestamp1=strtotime($rowobj->key_created_at);
+    $dateTimestamp2=strtotime(date('Y/m/d'));
+    if ($dateTimestamp1<$dateTimestamp2) {
+        http_response_code(201);
+        $JsonReq = array('code' => 100, 'message' => 'ApiKey expired!!!');
+        print json_encode($JsonReq);
+        exit; 
     }
 
     $filelist=array();
@@ -80,7 +96,7 @@
     }
     if ($Vi>MaxDatasets) {
         http_response_code(201);
-        $JsonReq = array('code' => 90, 'message' => 'Sorry, no more than 10 datasets are permitted per account!!!');
+        $JsonReq = array('code' => 110, 'message' => 'Sorry, no more than '.MaxDatasets.' datasets are permitted per account!!!');
         print json_encode($JsonReq);
         exit();
     }
@@ -90,7 +106,7 @@
     if (!is_dir($target_dir)) {
         if (!mkdir($target_dir, 0777, true)) {
             http_response_code(201);
-            $JsonReq = array('title' => 'Error', 'message' => 'Failed to store file!');
+            $JsonReq = array('code' => 120, 'message' => 'Failed to store file!');
             print json_encode($JsonReq);
             exit();
         }
@@ -100,7 +116,7 @@
     //if file already exists
     if (file_exists($target_file)) {
         http_response_code(201);
-        $JsonReq = array('title' => 'Exclamation', 'message' => $_FILES["fileToUpload"]["name"] . " already exists. ");
+        $JsonReq = array('code' => 130, 'message' => "File "  . $_FILES["fileToUpload"]["name"] . " already exists. ");
         print json_encode($JsonReq);
         exit();
     }
@@ -109,12 +125,12 @@
             move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file);
 
             http_response_code(200);
-            $JsonReq = array('title' => 'Information', 'message' => 'File stored successfully.');
+            $JsonReq = array('code' => 0, 'message' => 'File stored successfully.', 'datasetId' => $_POST['datasetType']."_".$_FILES["fileToUpload"]["name"]);
             print json_encode($JsonReq);
             exit();
         } catch (Exception $e) {
             http_response_code(400);
-            $JsonReq = array('title' => 'Exception', 'message' =>'Caught exception: ',  $e->getMessage(), "\n");
+            $JsonReq = array('code' => 140, 'message' =>'Caught exception: ',  $e->getMessage(), "\n");
             print json_encode($JsonReq);
             exit();
         }
