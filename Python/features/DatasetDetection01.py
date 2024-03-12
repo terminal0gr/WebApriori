b924ccbef32009c2683bb4d7c2e7c5f58c84afdb
Malliaridis Konstantinos <terminal_gr@yahoo.com>
2024-03-12 23:43:25 +0200

    Dataset type detection 95,2%

--- Python/features/DatasetDetection01.py
+++ Python/features/DatasetDetection01.py
@@ -50,58 +50,62 @@ rf_classifier = RandomForestClassifier(n_estimators=120)
 # Step 3: Perform ?-fold Cross-Validation
 split=6
 
-kf = KFold(n_splits=split, shuffle=True)
-fold = 1
+for i in range(10):
 
-acc_score = []
+    kf = KFold(n_splits=split, shuffle=True)
+    fold = 1
 
-for train_index, test_index in kf.split(X):
-    X_train, X_test = X.iloc[train_index,:],X.iloc[test_index,:]
-    y_train, y_test = y[train_index], y[test_index]
-    
-    # Train the model
-    rf_classifier.fit(X_train, y_train)
-    
-    # Predict on the test set
-    y_pred = rf_classifier.predict(X_test)
-    
-    acc = accuracy_score(y_pred , y_test)
-    acc_score.append(acc)
-    
-    print()
-    for row_index, (input, prediction, label) in enumerate(zip (X_test, y_pred, y_test)):
-      if prediction != label:
-      
-        print('Dataset', y_test.index[row_index], dsData['name'].iloc[y_test.index[row_index]], 'classified as ', prediction, 'should be ', label)
-      
-        strsql = "UPDATE `datasetwrongclassification` \
-                      set instances=instances+1 \
-                  Where `datasetName`=%s \
-                  And   `realClass`  =%s \
-                  And   `predictedClass`=%s"
-        queryParams=(dsData['name'].iloc[y_test.index[row_index]],int(label),int(prediction))
-        mySql.myQuery(strsql,queryParams)
+    acc_score = []
 
-        strsql = "INSERT INTO `datasetwrongclassification`(`datasetName`, `realClass`, `predictedClass`, `instances`)  \
-                  SELECT %s, %s, %s, 1 \
-                  WHERE NOT EXISTS (SELECT `datasetName` \
-                      FROM `datasetwrongclassification` \
-                      WHERE `datasetName`=%s \
-                      AND   `realClass`=%s \
-                      AND   `predictedClass`=%s)"
-        queryParams=(dsData['name'].iloc[y_test.index[row_index]],int(label),int(prediction),dsData['name'].iloc[y_test.index[row_index]],int(label),int(prediction))
-        mySql.myQuery(strsql,queryParams)
+    for train_index, test_index in kf.split(X):
+        X_train, X_test = X.iloc[train_index,:],X.iloc[test_index,:]
+        y_train, y_test = y[train_index], y[test_index]
+        
+        # Train the model
+        rf_classifier.fit(X_train, y_train)
+        
+        # Predict on the test set
+        y_pred = rf_classifier.predict(X_test)
+        
+        acc = accuracy_score(y_pred , y_test)
+        acc_score.append(acc)
+        
+        #print()
+        for row_index, (input, prediction, label) in enumerate(zip (X_test, y_pred, y_test)):
+          if prediction != label:
+        
+            #print('Dataset', y_test.index[row_index], dsData['name'].iloc[y_test.index[row_index]], 'classified as ', prediction, 'should be ', label)
+        
+            strsql = "UPDATE `datasetwrongclassification` \
+                        set instances=instances+1 \
+                    Where `datasetName`=%s \
+                    And   `realClass`  =%s \
+                    And   `predictedClass`=%s"
+            queryParams=(dsData['name'].iloc[y_test.index[row_index]],int(label),int(prediction))
+            mySql.myQuery(strsql,queryParams)
 
-    # Compute confusion matrix
-    cm = confusion_matrix(y_test, y_pred)
-    
-    # Print confusion matrix for the fold
-    print(f"Fold {fold} Accuracy {acc} Confusion Matrix:")
-    print(cm)
-    
-    # Increment fold counter
-    fold += 1
-    
-print('accuracy of each fold - {}'.format(acc_score))
-print('Avg accuracy : {}'.format(sum(acc_score)/split))
+            strsql = "INSERT INTO `datasetwrongclassification`(`datasetName`, `realClass`, `predictedClass`, `instances`)  \
+                    SELECT %s, %s, %s, 1 \
+                    WHERE NOT EXISTS (SELECT `datasetName` \
+                        FROM `datasetwrongclassification` \
+                        WHERE `datasetName`=%s \
+                        AND   `realClass`=%s \
+                        AND   `predictedClass`=%s)"
+            queryParams=(dsData['name'].iloc[y_test.index[row_index]],int(label),int(prediction),dsData['name'].iloc[y_test.index[row_index]],int(label),int(prediction))
+            mySql.myQuery(strsql,queryParams)
+
+        # Compute confusion matrix
+        #cm = confusion_matrix(y_test, y_pred)
+        
+        # Print confusion matrix for the fold
+        #print(f"Fold {fold} Accuracy {acc} Confusion Matrix:")
+        #print(cm)
+        
+        # Increment fold counter
+        fold += 1
+        
+    #percentages = ["{:.1f}%".format(number * 100) for number in acc_score]
+    #print('accuracy of each fold - {}'.format(percentages))
+    avgper="{:.1f}%".format(sum(acc_score)/split*100)
+    print(f'aa {i+1}: Avg accuracy : {avgper}')
 
