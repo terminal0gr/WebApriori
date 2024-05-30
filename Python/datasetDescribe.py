09a9b0d3f3671f702f67ebaf98c3b5dd7109b450
Malliaridis Konstantinos <terminal_gr@yahoo.com>
2024-05-31 00:54:01 +0300

    dataset Description 31/05/2024

--- Python/datasetDescribe.py
+++ Python/datasetDescribe.py
@@ -22,13 +22,6 @@ if len(sys.argv)>2:
     if len(sys.argv[2])>0:
         datasetName=sys.argv[2]
 
-datasetType=-1 # -1 means detect DatasetType. Else force the value from user
-if len(sys.argv)>3:
-	try:
-		datasetType=int(sys.argv[3])
-	except:
-		datasetType=-1 
-
 public=0 # 0 > private, 1 > public
 if len(sys.argv)>4:
     if len(sys.argv[4])>0:
@@ -37,7 +30,6 @@ if len(sys.argv)>4:
 #------------------------------           
 #end command line arguments section
 #------------------------------
-
 
 metadataInst=Metadata.Metadata()
 metaDataFile=metadataInst.readMetadataFile(identity,datasetName,public)
@@ -53,9 +45,9 @@ dataset = pd.read_csv(filepath, sep=metaDataFile['delimiter'])
 
 print(dataset.describe(include='all'))
 
-#datasetDescription['Shape']=f"Rows:{dataset.shape[0]}, Columns:{dataset.shape[1]}"
+datasetDescription['Shape']=f"Rows:{dataset.shape[0]}, Columns:{dataset.shape[1]}"
 datasetDescription['Describe']=dataset.describe(include='all').to_dict(orient='dict')
-#datasetDescription['Head']=dataset.head(10).to_dict(orient='records')
+datasetDescription['Head']=dataset.head(10).to_dict(orient='records')
 print(json.dumps(datasetDescription, indent=4))
 
 
