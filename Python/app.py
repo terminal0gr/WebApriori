2396b420e76cb8837def5a3a1c0ede38c17e9805
Malliaridis Konstantinos <terminal_gr@yahoo.com>
2024-09-05 16:17:07 +0300

    05/09/2024 MultiTasking

--- Python/app.py
+++ Python/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 import time
+import Main05
 
 # app = Flask(__name__)
 
@@ -19,9 +20,13 @@ def run_script():
     # Perform some long-running task
     # result = some_long_running_function()
     # return jsonify({"result": result})
-    time.sleep(10)  # Simulates a long-running task
-    return jsonify({"result": "Task completed!"})
+    a = request.form.get('a')
+    b = request.form.get('b')
+    print(f"Received: a={a}, b={b}")
+    time.sleep(6)  # Simulates a long-running task
+    # return jsonify({"result": "Task completed!"})
+    return f"Task completed! Received a={a}, b={b}"
 
 if __name__ == '__main__':
     from waitress import serve
-    serve(app, host='0.0.0.0', port=8080)
+    serve(app, host='0.0.0.0', port=8081)
--- php/app.php
+++ php/app.php
@@ -1,11 +1,21 @@
 <?php
+
+    $a="Lumen";
+    $b="Arben";
+
     $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, "http://localhost:8080/run-script");
+    curl_setopt($ch, CURLOPT_URL, "http://localhost:8081/run-script");
     curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array('a' => $a, 'b' => $b)));  // Passing data
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     $response = curl_exec($ch);
     curl_close($ch);
 
-    echo $response;
+    //echo $response;
+
+    http_response_code(202);
+    $jsonReq = array('title' => "no excuse" , 'message' => $response);
+    print json_encode($jsonReq);
+    exit();   
 
 ?>
