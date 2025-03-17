ea62b4d28276a3d3ce876dcbf857d90435ab403f
Malliaridis Konstantinos <terminal_gr@yahoo.com>
2025-03-17 12:43:09 +0200

    17/32025 Paper

--- Python/datasets/Experiments/HTK02.bat
+++ Python/datasets/Experiments/HTK02.bat
@@ -1,3 +1,6 @@
-call python HTKMiner_Test.py "pumsb.dat" "2000" " " "True" "True"
+call python HTKMiner_Test.py "webdocs.dat" "100" " " "False" "True" 10000
+call python HTKMiner_Test.py "webdocs.dat" "1000" " " "False" "True" 10000
+call python HTKMiner_Test.py "webdocs.dat" "10000" " " "True" "True" 30000
+call python HTKMiner_Test.py "webdocs.dat" "10000" " " "False" "True" 30000
 
 pause
